f6fae192c5a249d0815650fe6dc9208043b3758c
NoLifeKing <github-things@nolifeking85.tv>
2024-09-07 10:44:47 +0200

    feat: Lagt till sidor för de olika delarna. Texterna är inte kompletta.

---
+++ WeddingSite/Pages/Party.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingSite.Pages
+{
+    public class PartyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
