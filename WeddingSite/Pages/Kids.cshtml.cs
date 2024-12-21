d47cbedb63a2c40d265a46aef19256c03c606ec8
NoLifeKing <github-things@nolifeking85.tv>
2024-12-21 17:19:28 +0100

    feat: Page about Kids, more images

---
+++ WeddingSite/Pages/Kids.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingSite.Pages
+{
+    public class KidsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
